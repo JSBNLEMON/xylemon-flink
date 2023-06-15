7d26e9c771a1df3932bde6be46ea268f089f3c6b
xiayi <3461312457@qq.com>
2023-06-15 17:23:26 +0800

    flink java test

---
+++ src/main/java/org/xylemon/Hello.java
@@ -0,0 +1,14 @@
+package org.xylemon;
+
+/**
+ * Author: xiayi
+ * Date: 2023/6/15 17:22
+ */
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
